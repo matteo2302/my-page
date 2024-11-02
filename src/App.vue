3cb053131ca79a067f25666389cd811691ec5e13
matteo2302 <matteo.pizzichemi01@gmail.com>
2024-11-02 19:20:59 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: 'App'
+}
+
+</script>
+
+
+<template>
+  <main>
+    <HelloWorld />
+  </main>
+</template>
+
+
+
+<style scoped></style>
